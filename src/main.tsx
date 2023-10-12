153d8f2ec1f4b2757cac6a0c42a31536fdbf8896
GianfrancoDePace <depacegianfranco0@gmail.com>
2023-10-12 16:06:52 +0200

    included routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Componenti/Home/Home";
+import AboutMe from "./Componenti/AboutMe/AboutMe";
+import DataFetch from "./Componenti/AboutMe/DataFetcth/DataFetch";
+import Skills from "./Componenti/Skills/Skills";
+import Projects from "./Componenti/Projects/Projects";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="home" element={<Home />} />
+      <Route path="about" element={<AboutMe />} />
+        <Route path="fetch" element={<DataFetch />} />
+      <Route path="skills" element={<Skills />} />
+      <Route path="projects" element={<Projects />} />
+    </Routes>
+  </BrowserRouter >,
   document.getElementById("root")
 );
 
